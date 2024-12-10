377289318cfe7cccfac76193daa24fd34ac5af64
Arthur Batista <batistazco@gmail.com>
2024-12-10 19:58:10 -0300

    add some controllers, models and migrations

---
+++ database/migrations/2024_12_10_225026_create_loan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loan', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('books_id');
+            $table->timestamps();
+
+            $table->foreign('student_id')->references('id')->on('student');
+            $table->foreign('books_id')->references('id')->on('books');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loan');
+    }
+};
