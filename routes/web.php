7bdc49572eec8b010247385cc6949de816ca700c
Arthur Batista <batistazco@gmail.com>
2024-12-11 21:50:55 -0300

    configure web.php file with the application routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BooksController;
+use App\Http\Controllers\LoanController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('autor', AuthorController::class);
+Route::resource('livros', BooksController::class);
+Route::resource('alunos', StudentController::class);
+Route::resource('emprestimos', LoanController::class);
